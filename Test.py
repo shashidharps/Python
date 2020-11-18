c062d4fd5d9fb2020f462b0f2a6222c668501c1a
shashidharps <57847726+shashidharps@users.noreply.github.com>
2020-11-17 23:49:45 -0600

    Update Test.py

--- Test.py
+++ Test.py
@@ -4,11 +4,11 @@ fh = open(fname)
 count = 0
 
 for line in fh:
-    if 'select' in line and 'from' in line and 'object-type' not in line and 'ci type' not in line:
+    if 'select' in line and 'from' in line:
         # Get String after substring occurrence using partition() method
         line = line.partition('select')[2]
         print("select " + line)
-    elif 'SELECT' in line and 'FROM' in line and 'object-type' not in line and 'ci type' not in line:
+    elif 'SELECT' in line and 'FROM' in line:
         line = line.partition('SELECT')[2]
         print("select " + line)
     elif 'where' in line and '= ?' in line:
